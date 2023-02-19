9fd83bea309ae2dd77c4f6a94bde79bbc586895f
Koskinen, Jonne <n4052@student.jamk.fi>
2023-02-19 18:03:27 +0200

    course_information, step9

--- part2/course_information/src/components/Content.js
+++ part2/course_information/src/components/Content.js
@@ -5,7 +5,7 @@ const Content = ({ course }) => {
     console.log("Content props: ", course)
     return (
       <div>
-        {course.parts.map( part => <Part key={part.id} part={part} /> )}
+        {course.parts.map(part => <Part key={part.id} part={part} />)}
         <Total course={course} />
       </div>
     )
