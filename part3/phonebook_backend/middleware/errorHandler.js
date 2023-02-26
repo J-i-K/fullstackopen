a03bb9de0ed5913ca266206ef900f5e69c0df5c3
Koskinen, Jonne <n4052@student.jamk.fi>
2023-02-26 12:24:22 +0200

    phonebook_backend_database, linted

--- part3/phonebook_backend/middleware/errorHandler.js
+++ part3/phonebook_backend/middleware/errorHandler.js
@@ -1,16 +1,16 @@
 const errorHandler = (error, request, response, next) => {
-    // console.error(error.name)
-    // console.log(error._message)
-    // console.log(error.message)
+  // console.error(error.name)
+  // console.log(error._message)
+  // console.log(error.message)
   
-    if (error.name === 'CastError') {
-        return response.status(400).send({ error: 'malformatted id' })
-    }
-    if (error.name === 'ValidationError') {
-        return response.status(400).send({error: error.message})
-    }
-  
-    next(error)
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+  if (error.name === 'ValidationError') {
+    return response.status(400).send({error: error.message})
+  }
+
+  next(error)
 }
 
 module.exports = errorHandler
--- part3/phonebook_backend/models/contact.js
+++ part3/phonebook_backend/models/contact.js
@@ -8,32 +8,31 @@ const url = process.env.DB_uri
 
 mongoose.connect(url)
   .then(result => {
-    console.log('connected to MongoDB')
+    console.log('connected to MongoDB', result)
   })
   .catch((error) => {
     console.log('error connecting to MongoDB:', error.message)
   })
 
 const contactSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        minLength: [3, 'Minimum required name length is 3, got {VALUE}'],
-        required: true
+  name: {
+    type: String,
+    minLength: [3, 'Minimum required name length is 3, got {VALUE}'],
+    required: true
+  },
+  number: {
+    type: String,
+    validate: {
+      validator: function formatValidator (number) {
+        console.log('validating format')
+        if (number.indexOf('-') !== -1) {
+          return /\d{2,3}-\d+/.test(number)
+        } else {return true}
+      },
+      message: props => `${props.value} is not of expected format, if there is a hyphen then first part must have between 2 and 3 numbers!`
     },
-    number: {
-        type: String,
-        validate: {
-            validator: function formatValidator (number) {
-                console.log('validating format')
-                if (number.indexOf('-') !== -1) {
-                    return /\d{2,3}-\d+/.test(number)
-                }
-                else {return true}
-            },
-            message: props => `${props.value} is not of expected format, if there is a hyphen then first part must have between 2 and 3 numbers!`
-        },
-        minLength: [8, 'Expected length of number is 8!']
-    }
+    minLength: [8, 'Expected length of number is 8!']
+  }
 })
 
 contactSchema.set('toJSON', {
